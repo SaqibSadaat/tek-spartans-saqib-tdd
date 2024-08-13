7ef1de6e95fc3543d3164e297e5adc6f749597e8
test@test.com <test@test.com>
2024-08-12 17:29:59 -0700

    starting playground with testNG

---
+++ src/test/java/playground/MyFirstTest.java
@@ -0,0 +1,31 @@
+package playground;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MyFirstTest {
+
+    @BeforeMethod
+    public void runBeforeTest() {
+        System.out.println("This is Before Each Test");
+    }
+
+    @Test
+    public void myFirstTestMethod() {
+        System.out.println("Hello World");
+    }
+
+    @Test
+    public void mySecondTestMethod() {
+        System.out.println("This is My second test");
+    }
+
+    @AfterMethod
+    public void runAfterEachTEstMethod() {
+        System.out.println("This is running after Each Test");
+    }
+
+
+
+}
